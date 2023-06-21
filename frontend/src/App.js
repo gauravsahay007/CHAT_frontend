0938a5c6efedb1d2b134fbed10b42ac3efc03926
gauravsahay007 <gauravsahay2468@gmail.com>
2023-06-21 13:00:42 +0530

    App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {  Box } from '@mui/material'
+import Routers from "./Router"
+import io from "socket.io-client"
+
+export default function App() {
+  
+  return (
+    <React.StrictMode>
+      
+       <Routers/> 
+    
+
+      </React.StrictMode>
+    
+  )
+}
---
+++ frontend/src/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileModal from "./Components/Modals/profile";
+import SideBar from "./Components/Modals/SideBar";
+export default function Test(){
+    
+
+        return (
+            <div> 
+                <SideBar/>
+                
+            </div>
+          )
+  
+}
